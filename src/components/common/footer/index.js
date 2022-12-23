cd75ec65f236de8e8c1c8b507a35e7cf0c0de4cf
Abhishek singh chauahn <115978151+abhishek-06-singh@users.noreply.github.com>
2022-12-23 16:06:14 +0530

    made zomato clone

---
+++ src/components/common/footer/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./footer.css";
+
+const Footer = () => {
+  return (
+    <div className="absolute-center max-width">
+      Made with 🔥 by Abhishek Singh Chauhan
+    </div>
+  );
+};
+
+export default Footer;
